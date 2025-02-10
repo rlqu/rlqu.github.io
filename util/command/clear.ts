2b12e200bf412f5d8ff2156d2b3f4bd7296573fc
Jannik <jannik.developer02@gmail.com>
2025-02-10 22:34:49 +0100

    clear everything (ill start over...)

--- util/command/clear.ts
+++
@@ -1,6 +0,0 @@
-export function executeCommand() {
-    let console = document.getElementById("console")
-    if(console == null) return;
-    
-    console.innerHTML = "";
-}
--- util/command/echo.ts
+++
@@ -1,5 +0,0 @@
-import { newLine } from "../line_util.js";
-
-export function executeCommand(message: string) {
-    newLine("", message, "", "#d4d4d4");
-}
